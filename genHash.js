bdf0510fe5f763976a060a318cd5fda18e8b7e57
Pandelis Zembashis <info@pandelis.me>
2016-11-19 18:39:17 +0000

    working download

--- genHash.js
+++ genHash.js
@@ -2,8 +2,14 @@ const crypto = require('crypto')
 
 module.exports = {
   sha1: (string) => {
-    var shasum = crypto.createHash('sha1');
-    shasum.update();
-    console.log(shasum.digest('hex'));
-  }
+    var shasum = crypto.createHash('sha1')
+    shasum.update(string)
+    return shasum.digest('hex')
+  },
+  checksum: (str, algorithm, encoding) => {
+    return crypto
+        .createHash(algorithm || 'sha1')
+        .update(str, 'utf8')
+        .digest(encoding || 'hex')
+}
 };
--- index.js
+++ index.js
@@ -2,8 +2,24 @@ var express = require('express')
 var app = express()
 var url = require('url');
 
+var path = require('path')
+
 var http = require('http');
 var fs = require('fs');
+
+var validUrl = require('valid-url');
+
+var genHash = require('./genHash.js')
+
+//options for res.sendfile
+var options = {
+    root: __dirname + '/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -16,11 +32,11 @@ app.get('/cdn/*', (req, res) => {
 
   console.log(req.params[0])
 
-  getRemoteFile(fileUrl, (response) => {
+  getRemoteFile(fileUrl, (fileName) => {
+    console.log(fileName)
     console.log('ok')
+    res.sendFile(fileName, options)
   })
-
-  res.send('ok')
 })
 
 var parseFileURL = (urlin) => {
@@ -29,18 +45,27 @@ var parseFileURL = (urlin) => {
 }
 
 var getRemoteFile = (content, cb) => {
-  const fileName = 'file3.jpg'
+  const fileExtention = path.extname(content)
+  const fileName = genHash.sha1(content) + fileExtention
   var file = fs.createWriteStream(fileName)
   var request = http.get(content, (response) => {
     response.pipe(file)
     file.on('finish', () => {
-      file.close(cb);  // close() is async, call cb after close completes.
+      file.close(cb(fileName));  // close() is async, call cb after close completes.
     })
     .on('error', function(err) { // Handle errors
     fs.unlink(fileName); // Delete the file async. (But we don't check the result)
     if (cb) cb(err.message);
   })
   })
+}
+
+var isUrl = (url) => {
+  if (validUrl.isWebUri(url)){
+      return true
+  } else {
+      return false
+  }
 }
 
 app.listen(3000, () => {
