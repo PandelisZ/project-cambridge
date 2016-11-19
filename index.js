78fa0979e80208b595dfed68a22536b5591d6032
Pandelis Zembashis <info@pandelis.me>
2016-11-19 20:30:27 +0000

    aws version

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 var url = require('url');
-
+var port = process.env.PORT || 3000;
 var path = require('path')
 
 var http = require('http');
@@ -44,9 +44,37 @@ var parseFileURL = (urlin) => {
   return parsed
 }
 
+var compareFiles = (file) => {
+  const fileName = genFileName(file)
+  fs.readFile(fileName, function (err, data) {
+    if (err){
+      return false
+    }else {
+      if (checkChecksum(file) === genchecksum(data)) {
+        return true
+      }
+      else {
+        return false
+      }
+    }
+
+  })
+}
+
+console.log(compareFiles('http://5to9studios.com/font-awesome/css/font-awesome.min.css'))
+
+function genFileName(file){
+  return genHash.sha1(file) + path.extname(file)
+}
+
+var checkChecksum = (content) => {
+  var request = http.get(content, (response) =>{
+    return genHash.checksum(response)
+  })
+}
+
 var getRemoteFile = (content, cb) => {
-  const fileExtention = path.extname(content)
-  const fileName = genHash.sha1(content) + fileExtention
+  var fileName = genFileName(content)
   var file = fs.createWriteStream(fileName)
   var request = http.get(content, (response) => {
     response.pipe(file)
@@ -56,7 +84,7 @@ var getRemoteFile = (content, cb) => {
     .on('error', function(err) { // Handle errors
     fs.unlink(fileName); // Delete the file async. (But we don't check the result)
     if (cb) cb(err.message);
-  })
+    })
   })
 }
 
@@ -68,6 +96,6 @@ var isUrl = (url) => {
   }
 }
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Example app listening on port 3000!')
 })
